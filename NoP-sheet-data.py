8ee0bc6f46328beb0ee305e851735e89cca5ec64
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2023-12-08 09:26:35 +0100

    rework special args behavior

--- NoP-sheet-data.py
+++ NoP-sheet-data.py
@@ -17,47 +17,53 @@ for a in ARGS:
         args.append(a)
 
 authors_file = 'authors_others.txt'
+authors_count = 10
 if not args or help_args():
     print(os.path.basename(APP), 'author [author ...]')
     print()
     print('ERROR: Need a author as parameter!')
-    print(f'  pass ? to read 10 lines from {authors_file}')
+    print(f"  pass - as first argument to exclude url's already present on the sheet")
+    print(f'  pass ? to read {authors_count} lines from {authors_file}')
     print(f'  pass * to read all lines from {authors_file}')
     exit()
 
+exclude = args[0] == '-'
+if exclude:
+    args = args[1:]
+special = args[0] if args and args[0] in ['*','?'] else None
 
 authors_lst = []
 list_url_data = []
 list_authors_empty = []
 list_authors_error = []
-if len(args) == 1:
-    if args[0] == '?':
-        authors_lst = read_lines(authors_file, [])
-        args = authors_lst[:10]
-        if not args:
-            print(f'{authors_file} is empty.')
-            exit()
-    
-    elif args[0] == '*':
-        spreadsheets = ini_spreadsheets()
-        
-        args = read_lines(authors_file, [])
-        if not args:
-            print(f'{authors_file} is empty.')
-            exit()
-        
-        print('Google Sheets: retrive all url of present post...')
-        try:
-            list_url_data = spreadsheets.get('data!G:G')[1:]
-            list_url_data = set(r[0] for r in list_url_data if r)
-        except HttpError as err:
-            list_url_data = []
-            print(err)
-            input()
 
+if special == '?':
+    authors_lst = read_lines(authors_file, [])
+    args = authors_lst[:authors_count]
 
-for author in args:
+elif special == '*':
+    args = read_lines(authors_file, [])
+
+if special and not args:
+    print(f'{authors_file} is empty.')
+    exit()
+
+if exclude and args:
+    print('Google Sheets: retrive all url of present post...')
+    spreadsheets = ini_spreadsheets()
+    try:
+        list_url_data = spreadsheets.get('data!G:G')[1:]
+        list_url_data = set(r[0] for r in list_url_data if r)
+    except HttpError as err:
+        list_url_data = []
+        print(err)
+        input()
+
+args_length = len(args)
+for args_idx in range(args_length):
     print()
+    author = args[args_idx]
+    
     lines = []
     all_post = defaultdict(dict)
     
@@ -92,7 +98,7 @@ for author in args:
                 loop = False
             time.sleep(1)
     
-    run_animation(get_all_post, f'Loading Reddit post for {author}')
+    run_animation(get_all_post, f'Loading Reddit post for {author}'+ (f' [{args_idx+1}/{args_length}]' if args_length > 1 else '') )
     
     # filtre posts
     for subreddit in SUBREDDITS:
@@ -141,33 +147,33 @@ for author in args:
     for idx in range(len(lines)):
         lines[idx] = '\t'.join(lines[idx][1:])
     
-    if list_url_data:
-        subdir = 'all-authors'
-        os.makedirs(subdir, exist_ok=True)
-        if lines:
-            write_lines(os.path.join(subdir, f'{author}.csv'), lines)
-        else:
-            if not all_post:
-                lst = list_authors_error
-                filename = f'! ERROR author.csv'
-            else:
-                lst = list_authors_empty
-                filename = f'- empty author.csv'
-            lst.append(author)
-            write_lines(os.path.join(subdir, filename), lst)
-    
+    if special == '*':
+        subdir = 'multi-authors'
     else:
-        write_lines(f'{author}.csv', lines)
+        subdir = '.'
+    os.makedirs(subdir, exist_ok=True)
+    
+    if lines or not special:
+        write_lines(os.path.join(subdir, f'{author}.csv'), lines)
+    else:
+        if not all_post:
+            lst = list_authors_error
+            filename = f'! ERROR author.txt'
+        else:
+            lst = list_authors_empty
+            filename = f'- empty author.txt'
+        lst.append(author)
+        write_lines(os.path.join(subdir, filename), lst)
     
     msg = '' if all_post else '!!ERROR!!'
     print(f'Data extracted u/{author}.', 'Post found:', len(lines), msg)
     
-    if list_url_data:
+    if special == '*':
         i = random.randint(8,22)
         msg = f'Waiting {i} seconds (to appease reddit)...'
         print(msg, end='\r')
         time.sleep(i+round(random.random(),1))
         print(' '*len(msg), end='\r')
 
-if authors_lst:
-    write_lines(authors_file, authors_lst[10:])
+if special == '?':
+    write_lines(authors_file, authors_lst[authors_count:])
