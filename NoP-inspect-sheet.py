8bf39c32b6e150a1255ddb40673aabd5d1693997
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2024-04-04 12:22:15 +0200

    inspect wrong reddit url

--- NoP-inspect-sheet.py
+++ NoP-inspect-sheet.py
@@ -54,12 +54,16 @@ try:
     row_length = len(table[0])
     not_full_row = []
     url_map = defaultdict(list)
+    url_wrong = {}
     for idx, r in enumerate(table, 1):
         if len(r) != row_length:
             not_full_row.append(idx)
         
         if len(r)>6:
-            url_map[r[6]].append(idx)
+            url = r[6]
+            url_map[url].append(idx)
+            if 'new.reddit' in url or 'old.reddit' in url:
+                url_wrong[idx] = url
     
     if not_full_row:
         print('Row not fulled:')
@@ -82,6 +86,18 @@ try:
         for l in lines:
             print(f' {l}:{l}')
     
+    print()
+    
+    if url_wrong:
+        print('Wrong reddit url:')
+    else:
+        print('No wrong reddit url.')
+    
+    for l,url in url_wrong.items():
+        print(f' {l}:{l} => {url}')
+    
+    print()
+
 except HttpError as err:
     print(err)
     input()
