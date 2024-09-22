7e4500927d33c59f4c4273a29a4f5a89ed15c48b
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2024-09-22 11:23:01 +0200

    new-posts.py: option to retrieve SpacePaladin posts

--- new-posts.py
+++ new-posts.py
@@ -4,15 +4,18 @@ from common import HttpError, init_spreadsheets, parse_post_id, read_subreddit
 
 args = argparse.ArgumentParser(description='Retrive the data from r/NatureofPredators, and push it to the spreadsheets')
 args.add_argument('-a', '--all', '--dont-exclude-url', dest='exclude_url', action='store_false', help="Retrive all entry, don't exclude where the url of the post is already in the spreadsheets")
-args.add_argument('--nsfw', action='store_true', help='Inspect the NSFW sub')
+args_choice = args.add_mutually_exclusive_group()
+args_choice.add_argument('--nsfw', action='store_true', help='Inspect the NSFW sub')
+args_choice.add_argument('-sp', '--spacepaladin', action='store_true', help='Inspect SpacePaladin15 posts')
 args.add_argument('-csv', '--csv', type=str, nargs='?', default=False, help='Output into a CSV file')
 args.add_argument('-id', '--id', '--oldest-post-id', dest='oldest_post_id', type=str, help='id of the oldest post to check. If not specified, used the id stored on the spreadsheets.')
 args.add_argument('--no-emtpy-row', '--no-pending-emtpy-row', dest='no_emtpy_row', action='store_false', help="Don't add emtpy row at the end of the of the 'data' sheet")
 args.add_argument('--no-update-filtre', '--no-update-filtre-view', dest='no_update_filtre', action='store_false', help="Don't update the range of the filtre views")
 args = args.parse_args()
 
-
-def _last_post_name():
+def _last_post_name() -> str:
+    if args.spacepaladin:
+        return 'last-post-spacepaladin'
     return 'last-post' + ('-nsfw' if args.nsfw else '')
 
 def get_oldest_post_id() -> str:
@@ -52,12 +55,6 @@ def set_oldest_post_id(oldest_post: str):
     else:
         print('ERROR: No last-post line found')
 
-
-if args.nsfw:
-    subreddit='NatureOfPredatorsNSFW'
-else:
-    subreddit='NatureofPredators'
-
 if args.oldest_post_id:
     args.oldest_post_id = parse_post_id(args.oldest_post_id)
     print('Oldest post to check', args.oldest_post_id)
@@ -71,10 +68,26 @@ else:
         print('Google Sheets: no oldest post to check found')
 print()
 
+if args.spacepaladin:
+    subreddit = 'SpacePaladin15'
+    subreddit_is_author = True
+    kargs = {
+        'timeline_key_words':{'Main':['.+']}
+    }
+else:
+    if args.nsfw:
+        subreddit='NatureOfPredatorsNSFW'
+    else:
+        subreddit='NatureofPredators'
+    subreddit_is_author = False
+    kargs = {}
+
 lines = read_subreddit(
     subreddit=subreddit,
+    subreddit_is_author=subreddit_is_author,
     oldest_post=args.oldest_post_id,
     exclude_url=args.exclude_url,
+    **kargs,
 )
 
 if not lines:
